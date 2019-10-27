f73b6b0fa0653b2d94d475d89fb51e74ca951597
developrfw <developrfw@gmail.com>
2019-10-26 22:27:38 -0600

    finishedgit add .!

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => (
+  <div className="container">
+    <div className="jumbotron bg text-center">
+      <h1 className="logo">Clicky-Game!</h1>
+      <p className="desc">Click on Simpsons Pics!</p>
+    </div>
+  </div>
+);
+
+export default Header;
